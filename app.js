01ec694d406d60f3c8353c8e82b5b822f9ca0b41
mrbjones <mrbjones@users.noreply.github.com>
2015-11-10 15:25:11 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var http = require('http');
-var port = process.env.PORT;
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello Mr. Jones');
-}).listen(port);
+}).listen(process.env.VCAP_APP_PORT);
