32214b704a876adfa17942273ab1b8864144cd57
mrbjones <mrbjones@users.noreply.github.com>
2015-11-10 15:16:49 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var http = require('http');
-var port = parseInt(process.env.PORT,10);
+var port = process.env.PORT;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hellold\n');
-}).listen(port, '0.0.0.0');
-console.log('Serverning at http://0.0.0.0:' + port.toString() + '/');
+  res.end('Hello Mr. Jones');
+}).listen(port);
