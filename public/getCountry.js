3a4ed82258a273725b51c9c22b1c6e645d92772b
mrbjones <mrbjones@users.noreply.github.com>
2015-11-11 10:54:07 -0700

    Update and rename getHello.js to getCountry.js

---
+++ public/getCountry.js
@@ -0,0 +1,24 @@
+{
+"records": [
+  {
+    "Name" : "Alfreds Futterkiste",
+    "City" : "Berlin",
+    "Country" : "Germany"
+  },
+  {
+    "Name" : "Berglunds snabbköp",
+    "City" : "Luleå",
+    "Country" : "Sweden"
+  },
+  {
+    "Name" : "Centro comercial Moctezuma",
+    "City" : "México D.F.",
+    "Country" : "Mexico"
+  },
+  {
+    "Name" : "Ernst Handel",
+    "City" : "Graz",
+    "Country" : "Austria"
+  }
+  ]
+}
