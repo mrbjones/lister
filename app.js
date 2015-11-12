4b098c669f034382603f2e8fb0736e63985fb6c4
mrbjones <mrbjones@users.noreply.github.com>
2015-11-12 13:49:59 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ var http = require("http");
 
 if (process.env.VCAP_SERVICES) 
 {
-  var services = JSON.parse(process.env.VCAP_SERVICES);
+var services = JSON.parse(process.env.VCAP_SERVICES);
 var orchestrateConfig = services["orchestrate"];
 if (orchestrateConfig) {
     var node = orchestrateConfig[0];
 orchestrate_api_key = node.credentials.ORCHESTRATE_API_KEY
 orchestrate_api_url = node.credentials.ORCHESTRATE_API_HOST
   }
-
+};
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
