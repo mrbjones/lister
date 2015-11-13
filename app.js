eb3248e89b89daada760ee768370d205306b991e
mrbjones <mrbjones@users.noreply.github.com>
2015-11-13 07:57:32 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 var http = require("http");
-var orc = require("orchestrate");
+var db = require("orchestrate");
 
 
 if (process.env.VCAP_SERVICES) 
@@ -10,13 +10,26 @@ var orchestrateConfig = services["orchestrate"];
 if (orchestrateConfig) {
     var node = orchestrateConfig[0];
 orchestrate_api_key = node.credentials.ORCHESTRATE_API_KEY
-orchestrate_api_url = node.credentials.ORCHESTRATE_API_HOST
+orchestrate_api_endpoint = node.credentials.ORCHESTRATE_API_HOST
   }
 };
 
+db.put('cars', 'brettsvwgti', {
+  "make": "Volkswagen",
+  "model": "GTI S"
+  "color": "Black",
+  "year": "2015"
+})
+.then(function (result) {
+ response.write('success !')
+})
+.fail(function (err) {
+ response.write('fail /')
+})
+
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write(orchestrate_api_key)
-  response.write("Hello World");
+ 
+  response.write(" // ");
   response.end();
 }).listen(process.env.VCAP_APP_PORT);
