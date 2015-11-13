7bb64fcfc7b5b8d3805d69ffcb0a8a88cf3cf896
mrbjones <mrbjones@users.noreply.github.com>
2015-11-13 11:51:28 -0700

    Update app.js

--- app.js
+++ app.js
@@ -35,8 +35,10 @@ db.put('cars', 'brettsvwgti', {
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write('start<br>')
  putter( function(putt1)
- {response.write(putt1);response.end();     });
+ {response.write(putt1);  response.end();    });
+
   
   
 }).listen(process.env.VCAP_APP_PORT);
