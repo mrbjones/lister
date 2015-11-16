75c028aa6b737ca99fde844df70c614da082436e
mrbjones <mrbjones@users.noreply.github.com>
2015-11-16 13:35:54 -0700

    Update app.js

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ response.write("Here's some stuff!")
 response.write("</div>")
 response.write("<div ng-controller=\"ExampleController\" id=footer>")
 
-response.write("<form  novalidate class=\"simple-form\" ><table><tr><td>name</td><td><input name=key ng-model=\"carz.key\"><input type=hidden name=o value='p'  ng-model=\"carz.o\"></td></tr><tr><td>make</td><td><input name=make  ng-model=\"carz.make\"></td></tr><tr><td>model</td><td><input name=model  ng-model=\"carz.model\"></td></tr><tr><td>year</td><td><input name=year  ng-model=\"carz.year\"></td></tr><tr><td>color</td><td><input name=color  ng-model=\"carz.color\"></td></tr><tr><td colspan=2><input type=submit  ng-click=\"update(carz)\" value=\"Save\" ></td></tr></table></form>");
+response.write("<form  novalidate class=\"simple-form\" ><table><tr><td>name</td><td><input name=key ng-model=\"carz.key\"><input style=display:none; name=o value='p'  ng-model=\"carz.o\"></td></tr><tr><td>make</td><td><input name=make  ng-model=\"carz.make\"></td></tr><tr><td>model</td><td><input name=model  ng-model=\"carz.model\"></td></tr><tr><td>year</td><td><input name=year  ng-model=\"carz.year\"></td></tr><tr><td>color</td><td><input name=color  ng-model=\"carz.color\"></td></tr><tr><td colspan=2><input type=submit  ng-click=\"update(carz)\" value=\"Save\" ></td></tr></table></form>");
 /*
 response.write("<form  novalidate class=\"simple-form\" >name<input name=key ng-model=\"carz.key\"><input type=hidden name=o value='p'  ng-model=\"carz.o\">make<input name=make  ng-model=\"carz.make\">model<input name=model  ng-model=\"carz.model\">year<input name=year  ng-model=\"carz.year\">color<input name=color  ng-model=\"carz.color\"><input type=button  ng-click=\"update(carz)\" value=\"Save\" ></form>");
 */
