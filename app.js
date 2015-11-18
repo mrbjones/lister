08cfe6bc9cc0fa8f17d6c156b0a3962e233322f4
mrbjones <mrbjones@users.noreply.github.com>
2015-11-18 11:22:52 -0700

    Update app.js

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ response.write("<script src=\"//ajax.googleapis.com/ajax/libs/angularjs/1.5.0-be
 response.write("</head><body>");
 response.write("<br><br><div id=title>Brett's Car-o-rama!</div><br>")
 response.write("<br><br><div  ng-app=\"getter\" id=main ng-controller=\"ListCarz\">")
-response.write("<div ng-bind-html=\"myHTML\" ng-init=\"update(cars)\"></div>")
+response.write("<div ng-bind-html=\"cars.myHTML\" ng-init=\"update(cars)\"></div>")
 
 response.write("Here's some stuff!")
 
@@ -82,7 +82,7 @@ response.write("</div>")
 response.write("<script>  angular.module('putter', [])    .controller('ExampleController', ['$scope', '$http', function($scope,$http) {$scope.update = function(carz)  { $http({ url: 'http://btjweb1.uswest.appfog.ctl.io/app.js',    method: \"GET\",    params: {o: 'p',key: carz.key, make: carz.make, model: carz.model, color: carz.color, year:carz.year}}).success(function(data, status, headers, config) { alert(data) });   };     }]);")
 response.write("</script>")
 
-response.write("<script>  angular.module('getter', ['ngSanitize'])    .controller('ListCarz', ['$scope', '$http', function($scope,$http) {$scope.update = function(cars)  { $http({ url: 'http://btjweb1.uswest.appfog.ctl.io/app.js',    method: \"GET\",    params: {o: 'g'}}).success(function(data, status, headers, config) {alert(data);  myHTML = data });   };     }]);")
+response.write("<script>  angular.module('getter', ['ngSanitize'])    .controller('ListCarz', ['$scope', '$http', function($scope,$http) {$scope.update = function(cars)  { $http({ url: 'http://btjweb1.uswest.appfog.ctl.io/app.js',    method: \"GET\",    params: {o: 'g'}}).success(function(data, status, headers, config) {alert(data);  cars.myHTML = data });   };     }]);")
 response.write("</script>")
 
 response.end();
