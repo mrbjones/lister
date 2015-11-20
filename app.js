13b6aae2fb17b5091608a1c4f5342a82c3dc4b24
mrbjones <mrbjones@users.noreply.github.com>
2015-11-20 10:54:37 -0700

    Update app.js

--- app.js
+++ app.js
@@ -57,36 +57,29 @@ response.writeHeader(200, {"Content-Type": "text/html"});
 response.write("<!doctype html><html lang=\"en\"><head>  <meta charset=\"UTF-8\"><html><head><title>Brett's Car-o-rama!</title>  <script src=\"//ajax.googleapis.com/ajax/libs/angularjs/1.5.0-beta.1/angular.min.js\"></script>");
 response.write("<script src=\"//ajax.googleapis.com/ajax/libs/angularjs/1.5.0-beta.1/angular-sanitize.js\"></script>")
 
-response.write("</head><body>");
-response.write("<br><br><div id=title>Brett's Car-o-rama!</div><br>")
-response.write("<br><br><div  ng-app=\"getter\" id=main ng-controller=\"ListCarz\">")
+response.write("</head><body ng-app=\"putter\">");
+response.write("<br><br><div id=title>BTJ's Cars</div><br>")
+response.write("<br><br><div   id=main ng-controller=\"ListCarz\">")
 response.write("<div ng-init=\"update()\"></div>")
 
-response.write("<table><tr><td>details</td></tr><tr ng-repeat=\"car in carslist\">")
+response.write("<table><tr ng-repeat=\"car in carslist\">")
+response.write("<td>{{car.path.key}}</td>")
 response.write("<td>{{car.value.make}}</td>")
+response.write("<td>{{car.value.model}}</td>")
+response.write("<td>{{car.value.year}}</td>")
+response.write("<td>{{car.value.color}}</td>")
 response.write("</tr></table>")
 
-response.write("Here's some stuff!")
-
 response.write("</div>")
-response.write("<div ng-app=\"putter\" ng-controller=\"ExampleController\" id=footer>")
+response.write("<div  ng-controller=\"ExampleController\" id=footer>")
 
 response.write("<form  novalidate class=\"simple-form\" ><table><tr><td>name</td><td><input ng-model=\"carz.key\"><input style=display:none;  value='p'  ng-model=\"carz.o\"></td></tr><tr><td>make</td><td><input  ng-model=\"carz.make\"></td></tr><tr><td>model</td><td><input  ng-model=\"carz.model\"></td></tr><tr><td>year</td><td><input  ng-model=\"carz.year\"></td></tr><tr><td>color</td><td><input  ng-model=\"carz.color\"></td></tr><tr><td colspan=2><input type=button  ng-click=\"update(carz)\" value=\"Save\" ></td></tr></table></form>");
-/*
-response.write("<form  novalidate class=\"simple-form\" >name<input name=key ng-model=\"carz.key\"><input type=hidden name=o value='p'  ng-model=\"carz.o\">make<input name=make  ng-model=\"carz.make\">model<input name=model  ng-model=\"carz.model\">year<input name=year  ng-model=\"carz.year\">color<input name=color  ng-model=\"carz.color\"><input type=button  ng-click=\"update(carz)\" value=\"Save\" ></form>");
-response.write("<script>  angular.module('formExample', [])    .controller('ExampleController', ['$scope', function($scope) {   $scope.master = {};$scope.update = function(carz) { $scope.master = angular.copy(carz);};$scope.reset = function() {   $scope.carz = angular.copy($scope.master);}; $scope.reset();}]);</script>");
-response.write("<script>  angular.module('formExample', [])    .controller('ExampleController', ['$scope', function($scope) { $scope.master = {}; $scope.update = function(carz) {var m1=carz.make; alert(m1);  });     }]);")
-
-*/
-
 response.write("</div>")
 
 
 
 response.write("<script>  angular.module('putter', [])    .controller('ExampleController', ['$scope', '$http', function($scope,$http) {$scope.update = function(carz)  { $http({ url: 'http://btjweb2.uswest.appfog.ctl.io/app.js',    method: \"GET\",    params: {o: 'p',key: carz.key, make: carz.make, model: carz.model, color: carz.color, year:carz.year}}).success(function(data, status, headers, config) { alert(data) });   };     }]);")
-response.write("</script>")
-
-response.write("<script>  angular.module('getter', [])    .controller('ListCarz', ['$scope', '$http', function($scope,$http) {$scope.update = function()  { $http({ url: 'http://btjweb2.uswest.appfog.ctl.io/app.js',    method: \"GET\",    params: {o: 'g'}}).success(function(data) {$scope.carslist=[]; $scope.carslist=data });   };     }]);")
+response.write("   .controller('ListCarz', ['$scope', '$http', function($scope,$http) {$scope.update = function()  { $http({ url: 'http://btjweb2.uswest.appfog.ctl.io/app.js',    method: \"GET\",    params: {o: 'g'}}).success(function(data) {$scope.carslist=[]; $scope.carslist=data });   };     }]);")
 response.write("</script>")
 
 response.end();
